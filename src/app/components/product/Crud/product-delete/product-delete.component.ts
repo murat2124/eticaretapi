3d9190f470825c37fe6e7d429fd1797ef8364794
MURAT14530 <vegadost@hotmail.com>
2025-05-14 01:21:36 +0300

    v

--- src/app/components/product/Crud/product-delete/product-delete.component.ts
+++ src/app/components/product/Crud/product-delete/product-delete.component.ts
@@ -1,50 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Product } from 'src/app/Models/product';
 import { ProductService } from 'src/app/sevices/product.service';
 
 @Component({
   selector: 'app-product-delete',
   templateUrl: './product-delete.component.html'
 })
-export class ProductDeleteComponent {
+export class ProductDeleteComponent implements OnInit{
   productDeleteForm: FormGroup;
+
+  products:Product[]=[];
 
   constructor(
     private fb: FormBuilder,
     private productService: ProductService,
-    private router: Router
+    private router: Router,
+    private toastrService:ToastrService
   ) {
     this.productDeleteForm = this.fb.group({
       id: ['', Validators.required]
     });
   }
+  ngOnInit(): void {
+   this.getProduct();
+  }
 
-  // Ürünü silme işlemi
-  deleteProduct() {
-    const id = Number(this.productDeleteForm.value.id);
-    if (!id) {
-      alert("Lütfen geçerli bir ürün ID girin.");
-      return;
-    }
 
-    // Silme onayı alıyoruz
-    const confirmDelete = confirm("Bu ürünü silmek istediğinizden emin misiniz?");
-    if (!confirmDelete) return;
+  getProduct() {
+    this.productService.getallProducts().subscribe((response) => {
+      this.products = response.data;
 
-    // Silme işlemi servisi çağırıyoruz
-    this.productService.productDeleteS(id).subscribe({
-      next: () => {
-        alert('Ürün başarıyla silindi.');
-        this.productDeleteForm.reset();
-
-        // Silme işlemi sonrası ana sayfaya yönlendiriyoruz
-        this.router.navigate(['/']); // Ana sayfaya yönlendirme
-      },
-      error: () => {
-        alert('Silme işlemi başarısız oldu.');
-      }
+      this.toastrService.info("verileriniz geldi...")
     });
   }
+
+  // Ürünü silme işlemi
+ deleteProduct() {
+  const id = Number(this.productDeleteForm.value.id);
+  
+
+  
+
+  this.productService.productDeleteS(id).subscribe({
+    next: () => {
+      this.toastrService.success('Ürün başarıyla silindi.');
+      this.productDeleteForm.reset();
+      
+    },
+    error: () => this.toastrService.error('Silme işlemi başarısız oldu.')
+  });
+
+
+  
+  
+}
+
+
+
+
 }
