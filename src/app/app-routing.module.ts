d25c200147d0beb8585419d565dd1aaff1f9d764
MURAT14530 <vegadost@hotmail.com>
2025-05-13 21:39:02 +0300

    productupve produtdeleteok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductComponent } from './components/product/product.component';
 import { ProductAddComponent } from './components/product/Crud/product-add/product-add.component';
+import { ProductDeleteComponent } from './components/product/Crud/product-delete/product-delete.component';
+import { ProductUpdateComponent } from './components/product/Crud/product-update/product-update.component';
 
 const routes: Routes = [
 {path:"",pathMatch:"full",component:ProductComponent},
 {path:"Products",component:ProductComponent},
 {path:"products/category/:categoryId",component:ProductComponent},
 {path:"products/add",component:ProductAddComponent},
-{path:"products/update",component:ProductAddComponent},
-{path:"products/delete",component:ProductAddComponent}
+{path:"products/update",component:ProductUpdateComponent},
+{path:"products/delete",component:ProductDeleteComponent}
 
 
 
--- src/app/components/product/Crud/product-delete/product-delete.component.ts
+++ src/app/components/product/Crud/product-delete/product-delete.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-product-delete',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-delete.component.css']
 })
 export class ProductDeleteComponent {
+  
+  // Reactive form
+  productDeleteForm: FormGroup;
 
+  // Constructor
+  constructor(private fb: FormBuilder) {
+    // Form oluşturuluyor
+    this.productDeleteForm = this.fb.group({
+      id: ['', Validators.required] // ID alanı zorunlu
+    });
+  }
+
+  // Silme işlemi için form submit
+  onDeleteSubmit(): void {
+    if (this.productDeleteForm.valid) {
+      const productId = this.productDeleteForm.get('id')?.value;
+      // Burada silme işlemini gerçekleştirecek bir servis veya API çağrısı yapmalısınız.
+      console.log(`Product with ID ${productId} will be deleted.`);
+
+      // Örnek olarak başarılı bir silme işlemi için kullanıcıya bilgi verelim
+      alert(`Product with ID ${productId} has been deleted successfully!`);
+      
+      // Formu sıfırlayabiliriz
+      this.productDeleteForm.reset();
+    }
+  }
 }
